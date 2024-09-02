2ea0fe5a34d58b1c49e5438ece8538f47d02af2d
DHKim <doh09195@naver.com>
2024-09-03 01:16:02 +0900

    프래그먼트-리사이클러 뷰 연결 작업 미완

--- app/src/main/java/com/example/capstone_donkki/MainFragment.kt
+++ app/src/main/java/com/example/capstone_donkki/MainFragment.kt
@@ -5,18 +5,42 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.example.capstone_donkki.databinding.FragMainBinding
 
-class MainFragment: Fragment() {
-    private lateinit var mBinding: FragMainBinding
+class MainFragment : Fragment() {
+    private lateinit var mainBinding: FragMainBinding
+    private var layoutManager: RecyclerView.LayoutManager?=null
+    private var adapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>?=null
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        mBinding=FragMainBinding.inflate(inflater, container, false)
+        mainBinding = FragMainBinding.inflate(inflater, container, false)
 
-        return mBinding.root
+        return mainBinding.root
+    }
+
+    override fun onViewCreated(itemView: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(itemView, savedInstanceState)
+
+
+        //TODO: 프레그먼트 연결
+        val IngredientList = arrayListOf(
+            //추후 서버 연동 과정 필요
+            IngredientData(R.drawable.donkki_half, "식재료명", "0815"),
+            IngredientData(R.drawable.donkki_half, "식재료명", "0815")
+        )
+
+        val rcv_ingredient: RecyclerView = findViewById(R.id.rcv_ingredient)
+        rcv_ingredient.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        rcv_ingredient.setHasFixedSize(true)
+
+        //어뎁터 호출-연결
+        rcv_ingredient.adapter = Ingredient_Adapter(IngredientList)
+
     }
 }

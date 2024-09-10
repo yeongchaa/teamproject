ab9011eb1378e31a62a8cd91a0e5b99da945e7f7
최승호 <sh03_0506@naver.com>
2024-09-10 12:09:52 +0900

    배경색 설정

---
+++ app/src/main/java/csh/mjc/gagyebu/MainActivity.java
@@ -0,0 +1,14 @@
+package csh.mjc.gagyebu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
